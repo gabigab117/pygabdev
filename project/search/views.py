ae1d55985974d642755544f8b1af2c8d7cc7f6f5
gabigab117 <gabrieltrouve5@gmail.com>
2024-02-08 00:20:36 +0100

    add docstring

--- project/search/views.py
+++ project/search/views.py
@@ -5,17 +5,30 @@ from wagtail.contrib.search_promotions.models import Query
 
 
 def search_view(request):
-    # Search
+    """
+        Processes the search requests submitted through the GET method by looking up the 'query' parameter.
+        It performs a search across live pages in the Wagtail CMS and logs the search query for promoting results
+        in future searches.
+
+        If a search query is provided, it searches through the live pages using Wagtail's built-in search functionality,
+        logs the query for future reference, and collects the search results. If no query is provided, it returns an
+        empty queryset.
+
+        Args:
+            request (HttpRequest): The HTTP request object containing the search query.
+
+        Returns:
+            HttpResponse: The HTTP response object that renders the 'search/search.html' template, passing the search
+            query and the search results.
+        """
     search_query = request.GET.get('query', None)
     if search_query:
         search_results = Page.objects.live().search(search_query)
 
-        # Log the query so Wagtail can suggest promoted results
         Query.get(search_query).add_hit()
     else:
         search_results = Page.objects.none()
 
-    # Render template
     return render(request, 'search/search.html', {
         'search_query': search_query,
         'search_results': search_results,
